8fe9caeaf96ff8f8fe887cb487b5c204781e1256
eson-hub <chaesargiveson6@gmail.com>
2025-12-25 23:33:44 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# File utama menjalankan API server yang ada di file api.py
-
-import uvicorn
-
-def main():
-	uvicorn.run("api:app", host="0.0.0.0", port=8080, reload=True)
-
-if __name__ == "__main__":
-    main()
